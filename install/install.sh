e0196cf5fa45d95f734d39ffdc3e998b5253b33f
XezXey <puntawat1412@gmail.com>
2026-02-17 17:22:37 +0700

    affaaf

--- install/install.sh
+++ install/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-ln -sf ~/.vim/config/tmux-config/tmux.conf ~/.tmux.conf
-ln -sf ~/.vim/config/zshrc-config/zshrc ~/.zshrc
+ln -sf ~/.vim/install/config/tmux-config/tmux.conf ~/.tmux.conf
+ln -sf ~/.vim/install/config/zshrc-config/zshrc ~/.zshrc
 
 mkdir -p ~/.config/lf
-ln -sf ~/.vim/lfrc ~/.config/lf/lfrc
-ln -sf ~/.vim/lfcd.sh ~/.config/lf/lfcd.sh
+ln -sf ~/.vim/install/lfrc ~/.config/lf/lfrc
+ln -sf ~/.vim/install/lfcd.sh ~/.config/lf/lfcd.sh
 
 sudo npm i -g pyright vscode-langservers-extracted@latest
 nvim +PlugInstall +qall
