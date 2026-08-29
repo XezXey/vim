56be7fd6373558dfefd258aca70ba964edabde4a
Puntawat Ponglertnapakorn <puntawat1412@gmail.com>
2026-08-29 12:21:17 +0700

    Disable Copilot plugin configuration
    Comment out the configuration for the Copilot plugin.

--- install/config/nvim-config/lua/plugins.lua
+++ install/config/nvim-config/lua/plugins.lua
@@ -2324,55 +2324,55 @@ return {
       ]]
     end,
   },
-  { 'zbirenbaum/copilot.lua',
-    enabled = true and not in_singularity,
-    cmd = "Copilot",
-    event = "InsertEnter",
-    cond = not vim.g.vscode,
-    opts = {
-      panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-          jump_prev = "[[",
-          jump_next = "]]",
-          accept = "<CR>",
-          refresh = "gr",
-          open = "<M-CR>"
-        },
-        layout = {
-          position = "bottom", -- | top | left | right
-          ratio = 0.4
-        },
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<c-n>",
-          accept_word = false,
-          accept_line = false,
-          next = "<c-,>",
-          prev = "<c-.>",
-          dismiss = "<c-m>",
-        },
-      },
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-      },
-      copilot_node_command = 'node', -- Node.js version must be > 18.x
-      server_opts_overrides = {},
-    },
-  },
+  -- { 'zbirenbaum/copilot.lua',
+  --   enabled = true and not in_singularity,
+  --   cmd = "Copilot",
+  --   event = "InsertEnter",
+  --   cond = not vim.g.vscode,
+  --   opts = {
+  --     panel = {
+  --       enabled = true,
+  --       auto_refresh = false,
+  --       keymap = {
+  --         jump_prev = "[[",
+  --         jump_next = "]]",
+  --         accept = "<CR>",
+  --         refresh = "gr",
+  --         open = "<M-CR>"
+  --       },
+  --       layout = {
+  --         position = "bottom", -- | top | left | right
+  --         ratio = 0.4
+  --       },
+  --     },
+  --     suggestion = {
+  --       enabled = true,
+  --       auto_trigger = true,
+  --       debounce = 75,
+  --       keymap = {
+  --         accept = "<c-n>",
+  --         accept_word = false,
+  --         accept_line = false,
+  --         next = "<c-,>",
+  --         prev = "<c-.>",
+  --         dismiss = "<c-m>",
+  --       },
+  --     },
+  --     filetypes = {
+  --       yaml = false,
+  --       markdown = false,
+  --       help = false,
+  --       gitcommit = false,
+  --       gitrebase = false,
+  --       hgcommit = false,
+  --       svn = false,
+  --       cvs = false,
+  --       ["."] = false,
+  --     },
+  --     copilot_node_command = 'node', -- Node.js version must be > 18.x
+  --     server_opts_overrides = {},
+  --   },
+  -- },
   { 'olimorris/codecompanion.nvim',
     dependencies = {
       "nvim-lua/plenary.nvim",
